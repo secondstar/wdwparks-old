389fb0813cac8b667d1f05cf6e82c6f7adf8d4bd
captproton <carl@captproton.com>
2011-10-06 15:42:58 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdwparks::Application.config.secret_token = '282530d177a0e8553c11e30f3415387ff97e76fcff7657e1d33fc0906d6f69c2d0954afc2f37b1d3f6d5f1bc0b2e0ae3e86bf870c395fd1629d73e0838753301'
